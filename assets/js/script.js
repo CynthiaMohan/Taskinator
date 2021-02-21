e250bdb00da99893b6f0e9b8797d528a3c6c2782
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-02-21 11:35:07 -0800

    added to-do element and selected type

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,28 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event) {
     event.preventDefault();
+
+    var taskNameInput = document.querySelector('input[name="task-name"]').value;
+    // console.log(taskNameInput);
+
+    var taskTypeInput = document.querySelector('select[name="task-type"]').value;
+    console.log(taskTypeInput);
+
+    // Create a list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "this is a new task";
+
+    //Create the <div> to hold the task info and add the list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task=info";
+    //add inner html elements
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    console.log(taskInfoEl);
+
+    listItemEl.appendChild(taskInfoEl);
+    //appent the list item
     tasksToDoEl.appendChild(listItemEl);
+
 };
 
 // buttonEl.addEventListener("click", createTaskHandler);
